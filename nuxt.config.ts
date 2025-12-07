6f35d519e46d0206d7a91519379f996c7bbcc39e
art.rosas <kreates15@gmail.com>
2025-12-08 00:06:02 +0800

    Add Cloudflare configuration for asset binding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,15 @@ export default defineNuxtConfig({
     compatibilityDate: '2025-01-15'
   },
 
+   cloudflare: {
+    pages: {
+      assets: {
+        binding: 'PUBLIC_ASSETS' // <— any name except "ASSETS"
+      }
+    }
+  },
+
+
   content: {
     experimental: {
       sqliteConnector: 'native'
